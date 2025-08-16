61fa40bae2f8230218927055fcab0d8334b089c1
bastianDupoirieux <bast.dupo@gmail.com>
2025-08-16 18:36:50 +0200

    added ray deployment to model

--- model/load_model.py
+++ model/load_model.py
@@ -1,17 +1,42 @@
+from starlette.requests import Request
+
+import ray
+from ray import serve
+
 import torch
 from transformers import DistilBertForSequenceClassification, AutoTokenizer
+import yaml
 
 
-tokenizer=AutoTokenizer.from_pretrained('distilbert-base-uncased')
-model=DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased')
 
-def classify_text(text):
-    encoded_input=tokenizer(text, return_tensors='pt')
+with open('../config.yml', 'r') as stream:
+    try:
+        config = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
 
-    with torch.no_grad():
-        logits = model(**encoded_input).logits
+tokenizer = config['tokenizer']
+model = config['model']
 
-    predicted_class_id = logits.argmax().item()
-    predicted_label = model.config.id2label[predicted_class_id]
+@serve.deployment
+class ReviewClassifier:
+    def __init__(self):
+        self.tokenizer=AutoTokenizer.from_pretrained(tokenizer)
+        self.model = DistilBertForSequenceClassification.from_pretrained(model)
 
-    return predicted_label
+    def classify_text(self, text):
+        encoded_input=self.tokenizer(text, return_tensors='pt')
+
+        with torch.no_grad():
+            logits = self.model(**encoded_input).logits
+
+        predicted_class_id = logits.argmax().item()
+        predicted_label = self.model.config.id2label[predicted_class_id]
+
+        return predicted_label
+
+    async def __call__(self, http_request: Request):
+        review = await http_request.json()
+        return self.classify_text(review)
+
+classifier_app = ReviewClassifier.bind()
